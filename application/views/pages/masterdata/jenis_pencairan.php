28ec4d8298a02e1125b8dec85c776add3b91cadd
Ivanowsky Habeahan <ivanowskyfernandez@gmail.com>
2022-04-25 22:22:23 +0700

    update halaman jenis pencairan

--- application/views/pages/masterdata/jenis_pencairan.php
+++ application/views/pages/masterdata/jenis_pencairan.php
@@ -25,22 +25,18 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-12">
-                        <button type="button" class="btn btn-danger mb-2">Tambah Data</button>
+                        <a href="<?php echo base_url('dashboard/jenispencairantambah') ?>" class="btn btn-danger mb-2">Tambah Data</a>
                         <table class="table table-bordered">
                         <thead class="bg-danger">
                             <tr>
                                 <th style="width: 20px">No</th>
                                 <th>Nama</th>
-                                <th>Keterangan</th>
-                                <th style="">Status</th>
                                 <th style="width:40px">Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td>1.</td>
-                                <td></td>
-                                <td></td>
                                 <td></td>
                                 <td>
                                     <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle btn btn-danger"></a>
@@ -60,59 +56,6 @@
     </div>
 
 <!-- </section> --> 
-
-<section class="content" id="form">
-    <div class="card card-primary">
-        <div class="card-header">
-            <h3 class="card-title"><i class="fas fa-tachometer-alt"></i> &nbsp;Luaran : Tambah Data</h3>
-            <!-- Card-Tools -->
-            <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="card-refresh" data-source="widgets.html" data-source-selector="#card-refresh-content" data-load-on-init="false">
-                    <i class="fas fa-sync-alt"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="maximize">
-                    <i class="fas fa-expand"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                </button>
-            </div>
-            <!-- /.card-tools -->
-        </div>
-        <!-- /.card-header -->
-        <div class="card-body" style="display: block;">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-12">
-                        <form>
-                            <div class="form-group row">
-                                <label for="inputEmail3" class="col-sm-2 col-form-label">Nama</label>
-                                <div class="col-sm-10">
-                                <input type="email" class="form-control" id="inputEmail3" placeholder="Nama">
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="inputEmail3" class="col-sm-2 col-form-label">Keterangan</label>
-                                <div class="col-sm-10">
-                                <textarea name="" id="" cols="30" rows="5" class="form-control" placeholder="Keterangan"></textarea>                             
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <div class="col-sm-10 offset-sm-2 text-right">
-                                <button type="submit" class="btn btn-danger">Simpan Data</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-</section>
 
 <!-- Modal Delete-->
 <div class="modal fade" id="modal-hapus">
