f050df75d7448440f29795804de9a56d6028bc58
Samuel Halomoan Manalu <samman.r007@gmail.com>
2022-04-26 13:09:34 +0700

    Add luaran controllers

---
+++ application/controllers/Luaran.php
@@ -0,0 +1,113 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Luaran extends CI_Controller
+{
+
+	// Load database
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('luaran_model');
+	}
+
+	// View luaran
+	public function index()
+	{
+		$data	= array(
+			'title'		=> 'Master Data/Luaran',
+			'luaran'	=> $this->luaran_model->listing_luaran(),
+			'isi'		=> 'masterdata/luaran/list'
+		);
+		$this->load->view('layouts/dashboard', $data);
+	}
+
+	// public function tambah()
+	// {
+	// 	$data	= array(
+	// 				'title'		=> 'Tambah Luaran Baru',
+	// 				'isi'		=> 'masterdata/luaran/tambah'
+	// 			);
+	// 	$this->load->view('layouts/dashboard', $data);
+	// }
+
+	// Tambah luaran
+	public function tambah()
+	{
+		// Tambah luaran, check validasi
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('keterangan', 'Keterangan', 'required');
+		if ($this->form_validation->run() === FALSE) {
+			$data	= array(
+				'title'		=> 'Tambah Luaran Baru',
+				'isi'		=> 'masterdata/luaran/tambah'
+			);
+			$this->load->view('layouts/dashboard', $data);
+		} else {
+			$data = array(
+				'nama'			=> $this->input->post('nama'),
+				'keterangan' 	=> $this->input->post('keterangan'),
+			);
+			$this->luaran_model->tambah($data);
+			$this->session->set_flashdata('sukses', 'Data Luaran Berhasil Ditambah');
+			redirect(base_url() . 'index.php/luaran');
+		}
+	}
+
+	// Edit Luaran
+	public function edit($id)
+	{
+		$luaran = $this->luaran_model->listing_luaran($id);
+		// Tambah luaran, check validasi
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('keterangan', 'Keterangan', 'required');
+		if ($this->form_validation->run() === FALSE) {
+			$data	= array(
+				'title'		=> 'Edit Data Luaran',
+				'luaran'	=> $luaran,
+				'isi'		=> 'masterdata/luaran/edit'
+			);
+			$this->load->view('layouts/dashboard', $data);
+		} else {
+			$data = array(
+				'id'			=> $this->input->post('id'),
+				'nama'			=> $this->input->post('nama'),
+				'keterangan' 	=> $this->input->post('keterangan'),
+				'status'		=> $this->input->post('status')
+			);
+			$this->luaran_model->edit($data);
+			$this->session->set_flashdata('sukses', 'Data Luaran Berhasil Diupdate');
+			redirect(base_url() . 'index.php/luaran');
+		}
+	}
+
+	//Edit Status
+	public function	edit_status($id)
+	{
+		$luaran = $this->luaran_model->listing_luaran($id);
+
+		$status = $this->input->post('status');
+
+		if($status == 1){
+			$status = 0;
+		} else {
+			$status = 1;
+		}
+
+		$data = array(
+			'status'		=> $status,
+		);
+
+		$this->luaran_model->edit($data);
+		$this->session->set_flashdata('sukses', 'Status Berhasil Diupdate');
+		redirect(base_url() . 'index.php/luaran');
+	}
+
+	// Delete Luaran 
+	public function delete($id)
+	{
+		$data = array('id'	=> $id);
+		$this->luaran_model->delete($data);
+		$this->session->set_flashdata('sukses', 'Data Luaran Berhasil Dihapus');
+		redirect(base_url() . 'index.php/luaran');
+	}
+}
