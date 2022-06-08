16a4c20d072a3f39482fd2313a9d5a3e0684f75b
Ivanowsky Habeahan <ivanowskyfernandez@gmail.com>
2022-06-08 16:02:19 +0700

    update model sub bidang ilmu

---
+++ application/models/Sub_bidang_ilmu_model.php
@@ -0,0 +1,96 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Sub_bidang_ilmu_model extends CI_Model
+{
+	var $table = 'sub_bidang_ilmu';
+	var $column_search = array('nama', 'keterangan', 'status');
+	var $column_order = array('nama', 'keterangan', 'status');
+	// var $order = array('id' => 'asc');
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
+
+	private function _get_datatables_query($id)
+	{
+		$this->db->from('sub_bidang_ilmu');
+        $this->db->like('id_bidang_ilmu', $id);
+		$i = 0;
+
+		foreach ($this->column_search as $item) // loop column 
+		{
+			if ($_POST['search']['value']) // if datatable send POST for search
+			{
+
+				if ($i === 0) // first loop
+				{
+					$this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
+					$this->db->like($item, $_POST['search']['value']);
+				} else {
+					$this->db->or_like($item, $_POST['search']['value']);
+				}
+
+				if (count($this->column_search) - 1 == $i) //last loop
+					$this->db->group_end(); //close bracket
+			}
+			$i++;
+		}
+
+		if (isset($_POST['order'])) // here order processing
+		{
+			$this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+		} else if (isset($this->order)) {
+			$order = $this->order;
+			$this->db->order_by(key($order), $order[key($order)]);
+		}
+	}
+
+	function get_datatables($id)
+	{ 
+		$this->_get_datatables_query($id);
+		if ($_POST['length'] != -1)
+			$this->db->limit($_POST['length'], $_POST['start']);
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	function count_filtered($id)
+	{
+		$this->_get_datatables_query($id);
+		$query = $this->db->get();
+		return $query->num_rows();
+	}
+
+	function count_all()
+	{
+		$this->db->from('sub_bidang_ilmu');
+        // $this->db->like('id_bidang_ilmu', '3');
+		return $this->db->count_all_results();
+	}
+
+	function insert_sub_bidang_ilmu($table, $data)
+	{
+		$insert = $this->db->insert($table, $data);
+		return $insert;
+	}
+
+	function update_sub_bidang_ilmu($id, $data)
+	{
+		$this->db->where('id', $id);
+		$this->db->update('sub_bidang_ilmu', $data);
+	}
+
+	function get_sub_bidang_ilmu($id)
+	{
+		$this->db->where('id', $id);
+		return $this->db->get('sub_bidang_ilmu')->row();
+	}
+
+	function delete_sub_bidang_ilmu($id, $table)
+	{
+		$this->db->where('id', $id);
+		$this->db->delete($table);
+	}
+}
