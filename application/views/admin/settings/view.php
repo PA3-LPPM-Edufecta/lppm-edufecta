bb8a3fde7c9776c20bb5f0de87817d766798a9c7
Samuel Halomoan Manalu <samman.r007@gmail.com>
2022-06-16 07:04:23 +0700

    update settings view

--- application/views/admin/settings/view.php
+++ application/views/admin/settings/view.php
@@ -2,14 +2,14 @@
   <table style="width: 100%">
     <?php foreach ($data_table as $row) {
       foreach ($data_field as $key) {
-        if ($key->name != 'id') { ?>
+        if ($key->name != 'id' && $key->name != 'created_at' && $key->name != 'updated_at') { ?>
           <tr>
-            <td class="text-right" style="width: 50%;">
+            <td class="text-left" style="width: 50%;">
               <?= strtoupper($key->name); ?>
             </td>
             <td style="column-width: 10%">:</td>
             <td class="text-left" style="text-align: justify;">
-              <?= $row[$key->name]; ?>
+              &nbsp;<?= $row[$key->name]; ?>
             </td>
           </tr>
     <?php }
