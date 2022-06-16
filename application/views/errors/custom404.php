8960e45680de170741c52b488bfe493f4f71302a
Samuel Halomoan Manalu <samman.r007@gmail.com>
2022-06-16 07:04:06 +0700

    add custom 404 views

---
+++ application/views/errors/custom404.php
@@ -0,0 +1,10 @@
+<div class="text-center text-secondary">
+    <h1 class="display-1 text-danger font-title font-weight-bold">404</h1>
+    <h3 class="display-4 font-title">Halaman Tidak Ditemukan</h3>
+    <div>
+        Halaman yang kamu coba lihat tidak ada atau terjadi kesalahan lain.
+    </div>
+    <div>
+        <a href="<?php echo base_url('dashboard'); ?>" type="button" class="btn btn-outline-primary mt-3">Kembali ke Home</a>
+    </div>
+</div>
