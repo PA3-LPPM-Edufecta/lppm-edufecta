45dbeb59d75223b3d293f61c17ec3ce5c36eaa1d
Samuel Halomoan Manalu <44311690+Sam7Man@users.noreply.github.com>
2022-06-27 11:07:40 +0700

    add views custom403

---
+++ application/views/errors/custom403.php
@@ -0,0 +1,11 @@
+
+<div class="text-center text-secondary">
+    <h1 class="display-1 text-danger font-title font-weight-bold">403</h1>
+    <h3 class="display-4 font-title">Forbidden</h3>
+    <div>
+        Kamu tidak memiliki akses terhadap halaman ini.
+    </div>
+    <div>
+        <a href="<?php echo base_url('dashboard'); ?>" type="button" class="btn btn-outline-primary mt-3">Kembali ke Home</a>
+    </div>
+</div>
